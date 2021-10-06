0d1cf5406421a3f959f2f0e4735a0f0fc33e6f00
Theunis Victor <24910279@student.g.nwu.ac.za>
2021-10-06 19:21:55 +0200

    Create Date label on frmMain startup.

--- CMPG223 - Project/Form1.Designer.cs
+++ CMPG223 - Project/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace CMPG223___Project
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,35 @@ namespace CMPG223___Project
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDate
+            // 
+            this.lblDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(337, 36);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(140, 23);
+            this.lblDate.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(863, 539);
+            this.Controls.Add(this.lblDate);
+            this.Name = "frmMain";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
         }
 
+
         #endregion
+
+        private System.Windows.Forms.Label lblDate;
     }
 }
 
--- CMPG223 - Project/Form1.cs
+++ CMPG223 - Project/Form1.cs
@@ -10,11 +10,18 @@ using System.Windows.Forms;
 
 namespace CMPG223___Project
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
         }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            lblDate.Text = "Date: " + DateTime.Today;
+        }
+
+       
     }
 }
