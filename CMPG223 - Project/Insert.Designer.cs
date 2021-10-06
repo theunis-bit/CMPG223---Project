3ff8172079869c5cb04f1f52d0da97e36f71f8ab
Theunis Victor <24910279@student.g.nwu.ac.za>
2021-10-06 20:58:16 +0200

    Change Insert form to scholar

--- CMPG223 - Project/Insert.Designer.cs
+++ CMPG223 - Project/Insert.Designer.cs
@@ -34,10 +34,10 @@ namespace CMPG223___Project
             this.tbxStudentID = new System.Windows.Forms.TextBox();
             this.tbxName = new System.Windows.Forms.TextBox();
             this.tbxSurname = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblScholasrID = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblScholarDetails = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnAdd
@@ -79,51 +79,51 @@ namespace CMPG223___Project
             this.tbxSurname.Size = new System.Drawing.Size(165, 20);
             this.tbxSurname.TabIndex = 4;
             // 
-            // label1
+            // lblScholasrID
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 48);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Student ID: ";
+            this.lblScholasrID.AutoSize = true;
+            this.lblScholasrID.Location = new System.Drawing.Point(27, 48);
+            this.lblScholasrID.Name = "lblScholasrID";
+            this.lblScholasrID.Size = new System.Drawing.Size(63, 13);
+            this.lblScholasrID.TabIndex = 5;
+            this.lblScholasrID.Text = "Scholar ID: ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(27, 77);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.Size = new System.Drawing.Size(77, 13);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Student Name:";
+            this.label2.Text = "Scholar Name:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(27, 103);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.Size = new System.Drawing.Size(94, 13);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Student Surname: ";
+            this.label3.Text = "Scholar Surname: ";
             // 
-            // label4
+            // lblScholarDetails
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(30, 13);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(108, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Enter Student details:";
+            this.lblScholarDetails.AutoSize = true;
+            this.lblScholarDetails.Location = new System.Drawing.Point(30, 13);
+            this.lblScholarDetails.Name = "lblScholarDetails";
+            this.lblScholarDetails.Size = new System.Drawing.Size(107, 13);
+            this.lblScholarDetails.TabIndex = 8;
+            this.lblScholarDetails.Text = "Enter Scholar details:";
             // 
             // Insert
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(339, 191);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblScholarDetails);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblScholasrID);
             this.Controls.Add(this.tbxSurname);
             this.Controls.Add(this.tbxName);
             this.Controls.Add(this.tbxStudentID);
@@ -143,9 +143,9 @@ namespace CMPG223___Project
         private System.Windows.Forms.TextBox tbxStudentID;
         private System.Windows.Forms.TextBox tbxName;
         private System.Windows.Forms.TextBox tbxSurname;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblScholasrID;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblScholarDetails;
     }
 }
